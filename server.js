495dc396bd8b9626233f5f91d97f75ee61be2941
rhaynes3k <rhaynes3k@gmail.com>
2016-06-15 16:33:21 -0500

    firstcommit

--- server.js
+++ server.js
@@ -1,1 +1,54 @@
+const express = require('express');
+const {
+    json
+} = require('body-parser');
+const app = express();
+const port = 9000;
+app.use(json());
+let messages = [`WTF?!!`, `hello`];
 
+app.options('/', (req, res) => {
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }).send()
+});
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+
+app.post('/', (req, res) => {
+    messages.push({
+  message: req.body.message,
+  time: new Date()
+});
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+    // console.log(req.body);
+});
+
+
+app.listen(port, () => {
+    console.log('Gettin it in on port ' + port);
+});
